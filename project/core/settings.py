7a2959f916cb83b37831a334f98492d46f9ebebd
suhailvs <suhailvs@gmail.com>
2022-10-26 12:45:12 +0530

    added static files

--- project/core/settings.py
+++ project/core/settings.py
@@ -121,8 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+import os
 
-STATIC_URL = "/staticfiles/"
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'core/staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
